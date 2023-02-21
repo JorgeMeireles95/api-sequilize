b0892ba46133470ec0a330180591fc7f3d046829
Jorge Meireles <jorgeantoniomeireles@gmail.com>
2023-02-21 20:27:56 -0300

    inserting data into the database

--- api/seeders/20230221230301-demo_seed_pessoa.js
+++ api/seeders/20230221230301-demo_seed_pessoa.js
@@ -2,12 +2,18 @@
 
 //Para gerar um seed o comando:
 //npx sequelize-cli seed:generate --name demo_seed_pessoa
+
+//sicronizar o seed com o banco de dados
+//npx sequelize-cli db:seed:all
+
+
 //só terá qe gerar o seed uma vez na mão, depois o seed popula a tabela automaticamente
 //para desfazer o seed
 //npx sequelize-cli db:seed:undo ,somente desfaz o ultimo seed
 
 //para fazer de uma tabela especifica
 //npx sequelize-cli db:seed:undo --seed nome-do-arquivo
+
 
 //para desfazer todos os seeds
 //npx sequelize-cli db:seed:undo:all
@@ -24,10 +30,57 @@ module.exports = {
   //inserir dados na tabela
   async up (queryInterface, Sequelize) {
 
-      await queryInterface.bulkInsert('Pessoas', [{
-       name: 'John Doe',
-        isBetaMember: false
-      }], {});
+      await queryInterface.bulkInsert('Pessoas', [
+        {
+          nome: 'Ana Souza',
+          ativo: true,
+          email: 'ana@ana.com',
+          role: 'estudante',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          nome: 'Marcos Cintra',
+          ativo: true,
+          email: 'marcos@marcos.com',
+          role: 'estudante',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          nome: 'Felipe Cardoso',
+          ativo: true,
+          email: 'felipe@felipe.com',
+          role: 'estudante',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          nome: 'Sandra Gomes',
+          ativo: false,
+          email: 'sandra@sandra.com',
+          role: 'estudante',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          nome: 'Paula Morais',
+          ativo: true,
+          email: 'paula@paula.com',
+          role: 'docente',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          nome: 'Sergio Lopes',
+          ativo: true,
+          email: 'sergio@sergio.com',
+          role: 'docente',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+  
+     ], {});
     
   },
 
