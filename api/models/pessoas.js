8f24551a71ad12d3287bf451e358bd95daa30888
Jorge Meireles <jorgeantoniomeireles@gmail.com>
2023-02-21 20:16:27 -0300

    person template creation

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -1,17 +1,44 @@
 
-//tabela criado apartir do comando npx sequelize-cli model:create --name Pessoas --attributes nome:string,ativo:boolean,email:string,role:string
+//npx sequelize-cli model:create --name Pessoas --attributes nome:string,ativo:boolean,email:string,role:strings --attributes nome:string,ativo:boolean,email:string,role:string
+
 'use strict';
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  const Pessoas = sequelize.define('Pessoas', {
+  class Pessoas extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Pessoas.init({
     nome: DataTypes.STRING,
     ativo: DataTypes.BOOLEAN,
     email: DataTypes.STRING,
     role: DataTypes.STRING
-  }, {});
-  Pessoas.associate = function(models) {
-    // associations can be defined here
-  };
+  }, {
+    sequelize,
+    modelName: 'Pessoas',
+  });
   return Pessoas;
 };
 
+//para impedir que o sequelize crie o nome da tabela no plural, basta adicionar o seguinte código no arquivo de configuração do sequelize
+// sequelize.define('User', {
+//   // ... (attributes)
+// }, {
+//   freezeTableName: true
+// });
 
+
+//para definir o nome na tabela que ser usada no banco usar o tableName
+// sequelize.define('User', {
+//   // ... (attributes)
+// }, {
+//   tableName: 'Employees'
+// });
