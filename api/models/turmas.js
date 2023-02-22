5eb2f81d2322bfb0b19c5dc8121267a51259fc43
Jorge Meireles <jorgeantoniomeireles@gmail.com>
2023-02-22 02:23:53 -0300

    finishing making the associations between the tables

--- api/models/turmas.js
+++ api/models/turmas.js
@@ -10,7 +10,12 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+     Turmas.hasMany(models.Matriculas,{
+      foreignKey: 'turma_id'
+     })
+
+     Turmas.belongsTo(models.Pessoas) // fazendo o outro lado da relação
+     Turmas.belongsTo(models.Niveis) // fazendo o outro lado da relação
     }
   }
   Turmas.init({
