24acd300e03714800d5744c6ab369e526ca4ccdc
Jorge Meireles <jorgeantoniomeireles@gmail.com>
2023-02-22 02:07:11 -0300

    adding route pessoas in index

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const  pessoasRoute  = require( '../Nova pasta/api/routes/pessoasRoute.js')
+
 
 const app = express()
+const port = 3000
+
 
 app.use(bodyParser.json())
 
-const port = 3000
+app.use(pessoasRoute); //permitindo o express js usar o arquivo pessoasRoute.js
 
-app.get('/teste', (req, res) => res
-  .status(200)
-  .send({ mensagem: 'boas-vindas à API' 
-}))
+
+
 
 app.listen(port, () => console.log(`servidor está rodando na porta ${port}`))
 
-module.exports = app
+
